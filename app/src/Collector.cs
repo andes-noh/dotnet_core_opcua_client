5d68da4d8a302918d1f01e534670d9da3e56548b
andes-noh <nkj9693@gmail.com>
2022-09-14 11:50:50 +0900

    chore(collector): opcua 연결 및 데이터 수집 & 확인

--- app/src/Collector.cs
+++ app/src/Collector.cs
@@ -1,16 +1,20 @@
 namespace sample;
 
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Opc.Ua;
+using Client;
 
 public class Collector : BackgroundService
 {
+    OpcUaClient opcUaClient;
     public class Props
     {
-        public string? text { get; init; }
+        public string? endpointURL { get; set; }
     }
     private readonly Props _props;
 
@@ -18,8 +22,9 @@ public class Collector : BackgroundService
     {
         var props = new Props
         {
-            text = config["TEXT"],
+            endpointURL = "opc.tcp://" + config["ENDPOINT_URL"] + ":4840",
         };
+
         return new Collector(props);
     }
 
@@ -28,36 +33,76 @@ public class Collector : BackgroundService
         _props = props;
     }
 
-    public void HelloWorld(CancellationToken cancellationToken, string text)
+    public void DataCollector(CancellationToken cancellationToken, string endpoint)
     {
         while (!cancellationToken.IsCancellationRequested)
         {
             try
             {
-                // OpcUaClient opcUaClient = new OpcUaClient();
-                // opcUaClient.UserIdentity = new UserIdentity("OpcUaClient", "12345678");
-                // opcUaClient.ConnectServer("opc.tcp://192.168.10.21:4840");
-                // string value = opcUaClient.ReadNode<string>("ns=2;s=/Channel/ProgramInfo/progName");
-                // Console.WriteLine("ns=2;s=Machines/Machine B/TestValueFloat" + "   value: " + value);
-                // opcUaClient.Disconnect();
-                Thread.Sleep(1000);
+                if (!opcUaClient.getConnected)
+                {
+                    try
+                    {
+                        opcUaClient.UserIdentity = new UserIdentity("OpcUaClient", "12345678");
+                        opcUaClient.ConnectServer(endpoint);
+                        Thread.Sleep(10000);
+                    }
+                    catch (Exception e)
+                    {
+                        try
+                        {
+                            opcUaClient.setConnected(false);
+                            Thread.Sleep(5000);
+                        }
+                        catch (Exception e2)
+                        {
+                            try
+                            {
+                                opcUaClient.Disconnect();
+                                Thread.Sleep(5000);
+                            }
+                            catch (Exception e3)
+                            {
+                                opcUaClient.setConnected(false);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    List<NodeId> nodeIds = new List<NodeId>();
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/State/acProg"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/ProgramInfo/progName"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/Spindle/driveLoad"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/Spindle/actSpeed"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/Spindle/speedOvr"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/MachineAxis/feedRateOvr"));
+                    nodeIds.Add(new NodeId("ns=2;s=/Channel/State/acProg"));
+
+                    List<DataValue> dataValues = opcUaClient.ReadValues(nodeIds.ToArray());
+
+                    for (int i = 0; i < dataValues.Count; i++)
+                    {
+                        Console.WriteLine($"data{i + 1}: " + dataValues[i].ToString());
+                    }
+
+                    Thread.Sleep(5000);
+                }
+
             }
             catch (Exception)
             {
                 Console.WriteLine("Failed to read value");
             }
             // Console.WriteLine($"Sample Project: {text}");
-
         }
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        // func
-        // loop
-
-        Task.Run(() => HelloWorld(stoppingToken, _props.text));
-
+        opcUaClient = new OpcUaClient();
+        Console.WriteLine(_props.endpointURL);
+        Task.Run(() => DataCollector(stoppingToken, _props.endpointURL));
         return Task.CompletedTask;
     }
 
@@ -65,8 +110,7 @@ public class Collector : BackgroundService
     // BackgroundService는 생략가능
     public override Task StopAsync(CancellationToken cancellationToken)
     {
-
-
+        opcUaClient.Disconnect();
         return Task.CompletedTask;
     }
 }
